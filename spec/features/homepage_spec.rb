93b218aa09762de08ed5b0393edea628b9ea88c4
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-08-28 10:55:28 -0600

    Changed tests to proper javascript tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,10 +1,23 @@
 require 'rails_helper'
 
-describe "viewing the homepage" do
-  it "shows rails and js rendered text" do
+describe "Todos" do
+  it "allows a user to do add todos", js: true do
     visit root_path
 
-    expect(page).to have_content("Hello, I'm a normal rails view")
-    expect(page).to have_content("hello world, I'm from only javascript!")
+    expect(page).to have_content "Todoly"
+
+    fill_in "todo", with: "feed kittens"
+    click_button "Create Todo"
+
+    within "#todos-list" do
+       expect(page).to have_content "feed kittens"
+    end
   end
 end
+
+
+#these tests don't know how to execute javascript, there's just an empty html
+#page thats it trying to execute, not looking at js page
+
+
+#selenium webdriver is why firefox comes up
